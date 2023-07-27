0e42da0ee7140e42fa46c8c73e0b030d313df324
ahmedhussein74 <ahmedhusseinofficial22@gmail.com>
2023-07-27 13:19:50 +0300

    update index

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ require("./routes/lectureRoute")(app);
 require("./routes/enrollmentRoute")(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
